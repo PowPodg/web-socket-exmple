c0f5e3d1ed67e2414c583b9c94a5b0b650133b84
PowPodg <oleg@oleg-iv.tk>
2019-12-19 18:50:32 +0500

    edit

--- client/src/Consts/consts.js
+++ client/src/Consts/consts.js
@@ -1,5 +1,5 @@
 module.exports = {
-    URL_WEBSERV_WINSOCK: 'wss://echo.websocket.org/',
+    URL_WEBSERV_WINSOCK: 'http://web-socket-examp.herokuapp.com/',
     NO_CONNECT: 'No connect',
     CONNECTED: 'connected',
     ICON_IMAGE: 'img/m.jpg'
